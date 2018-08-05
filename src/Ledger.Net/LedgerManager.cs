9b69840c35fbe8064db99d60f80e61a252a809c4
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-08-06 07:22:35 +1000

    Constructor

---
+++ src/Ledger.Net/LedgerManager.cs
@@ -0,0 +1,18 @@
+﻿using Hid.Net;
+
+namespace Ledger.Net
+{
+    public class LedgerManager
+    {
+        #region Fields
+        private IHidDevice _LedgerHidDevice;
+        #endregion
+
+        #region Constructor
+        public LedgerManager(IHidDevice ledgerHidDevice)
+        {
+            _LedgerHidDevice = ledgerHidDevice;
+        }
+        #endregion
+    }
+}
