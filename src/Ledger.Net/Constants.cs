a7a9b9ad5f90b97557b2a350dde7c54a51822ce3
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-08-06 08:18:19 +1000

    A bit more

--- src/Ledger.Net/Constants.cs
+++ src/Ledger.Net/Constants.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Ledger.Net
+﻿namespace Ledger.Net
 {
     public class Constants
     {
         #region Generic App Constants
-        public byte CLA = 0xE0;
+        public const byte CLA = 0xE0;
+        #endregion
+
+        #region Bitcoin Blue Instructions
+        public const byte BTCHIP_INS_GET_WALLET_PUBLIC_KEY = 0x40;
         #endregion
     }
 }
---
+++ src/Ledger.Net/Requests/BitcoinAppGetPublicKeyRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ledger.Net.Requests
+{
+    public class BitcoinAppGetPublicKeyRequest : RequestBase
+    {
+        public override byte Argument1 => throw new NotImplementedException();
+
+        public override byte Argument2 => throw new NotImplementedException();
+
+        public override byte Cla => Constants.CLA;
+
+        public override byte Ins => Constants.BTCHIP_INS_GET_WALLET_PUBLIC_KEY;
+    }
+}
