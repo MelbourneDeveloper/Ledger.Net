64f4dc908f21b643a93aaa317d894528b714e975
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-08-06 07:55:13 +1000

    Add request base

---
+++ src/Ledger.Net/LedgerRequestBase.cs
@@ -0,0 +1,19 @@
+﻿namespace Ledger.Net
+{
+    public abstract class LedgerRequestBase
+    {
+        #region Public Abstract Properties
+        public abstract object Argument1 { get; }
+        public abstract object Argument2 { get; }
+        public abstract byte Cla { get; }
+        public abstract byte Ins { get; }
+        #endregion
+
+        #region Public Properties
+        #endregion
+
+        public LedgerRequestBase()
+        {
+        }
+    }
+}
