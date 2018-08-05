e79702294702a5c289f4803028cfa41198f0c396
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-08-06 07:59:29 +1000

    A bit more

--- src/Ledger.Net/LedgerRequestBase.cs
+++ src/Ledger.Net/LedgerRequestBase.cs
@@ -3,13 +3,14 @@
     public abstract class LedgerRequestBase
     {
         #region Public Abstract Properties
-        public abstract object Argument1 { get; }
-        public abstract object Argument2 { get; }
+        public abstract byte Argument1 { get; }
+        public abstract byte Argument2 { get; }
         public abstract byte Cla { get; }
         public abstract byte Ins { get; }
         #endregion
 
         #region Public Properties
+        public byte[] Data { get; }
         #endregion
 
         public LedgerRequestBase()
