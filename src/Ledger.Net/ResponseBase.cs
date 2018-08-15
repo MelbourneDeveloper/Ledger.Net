ef9df7ee9f6e233ac7975505b14768eac134614d
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-08-15 20:55:49 +1000

    Return code stuff

--- src/Ledger.Net/ResponseBase.cs
+++ src/Ledger.Net/ResponseBase.cs
@@ -9,13 +9,15 @@
         #region Public Properties
         public byte[] Data { get; }
         public int StatusCode { get; }
+        public bool IsSuccess => ReturnCode == 0x9000;
+        public int ReturnCode { get;  }
         #endregion
 
         #region Constructor
         public ResponseBase(byte[] data)
         {
             Data = data;
-            var ReturnCode = ((data[data.Length - 2] & HardeningConstant) << 8) | data[data.Length - 1] & HardeningConstant;
+            ReturnCode = ((data[data.Length - 2] & HardeningConstant) << 8) | data[data.Length - 1] & HardeningConstant;
         }
         #endregion
     }
