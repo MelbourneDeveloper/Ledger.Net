c05e256c47fc309a2c978e856e9bd5b475afc9c5
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-08-15 21:29:46 +1000

    A little closer

--- src/Ledger.Net/ResponseBase.cs
+++ src/Ledger.Net/ResponseBase.cs
@@ -10,8 +10,27 @@
         public byte[] Data { get; }
         public int StatusCode { get; }
         public bool IsSuccess => ReturnCode == 0x9000;
-        public int ReturnCode { get;  }
+        public int ReturnCode { get; }
+        public string StatusMessage
+        {
+            get
+            {
+                switch (ReturnCode)
+                {
+                    case 0x9000:
+                        return "Success";
+                    case 0x6D00:
+                        return "Instruction not supported in current app or there is no app running";
+                    case 0x6B00:
+                        return "Invalid parameter";
+                    default:
+                        return "Shrugging in your general direction";
+                }
+            }
+        }
         #endregion
+
+        private int asdasd =  0x6804;
 
         #region Constructor
         public ResponseBase(byte[] data)
