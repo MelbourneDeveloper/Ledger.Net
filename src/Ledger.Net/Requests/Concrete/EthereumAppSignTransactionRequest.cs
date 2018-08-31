364d7d3e3defd34755f52be6a04d32c60d59ecbc
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-09-01 09:21:06 +1000

    Add the request/response

---
+++ src/Ledger.Net/Requests/Concrete/EthereumAppSignTransactionRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace Ledger.Net.Requests
+{
+    public class EthereumAppSignTransactionRequest : RequestBase
+    {
+        #region Public Overrides
+        public override byte Argument1 => 0;
+        public override byte Argument2 => 0;
+        public override byte Cla => Constants.CLA;
+        public override byte Ins => Constants.ETHEREUM_SIGN_TX;
+        #endregion
+
+        #region Public Properties
+        #endregion
+
+        #region Constructor
+        public EthereumAppSignTransactionRequest(byte[] data) : base(data)
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ src/Ledger.Net/Responses/Concrete/EthereumAppSignTransactionResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Ledger.Net.Responses
+{
+    public class EthereumAppSignTransactionResponse : ResponseBase
+    {
+        public EthereumAppSignTransactionResponse(byte[] data) : base(data)
+        {
+        }
+    }
+}
