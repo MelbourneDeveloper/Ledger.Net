c0e1589e1c1637cecff69efc7ae87601e8b27744
Matthew LeLiever <matthew121250@hotmail.com>
2018-09-01 15:54:58 -0400

    Comments

--- src/Ledger.Net/Responses/Concrete/EthereumAppSignTransactionResponse.cs
+++ src/Ledger.Net/Responses/Concrete/EthereumAppSignTransactionResponse.cs
@@ -4,6 +4,10 @@ namespace Ledger.Net.Responses
 {
     public class EthereumAppSignTransactionResponse : ResponseBase
     {
+        // https://github.com/LedgerHQ/ledger-app-eth/blob/master/doc/ethapp.asc
+        // See "SIGN ETH TRANSACTION" section for the output.
+        // This is most likely correct, however correct transaction signing appearing on the device is still needed.
+
         public uint SignatureV { get; }
 
         public byte[] SignatureR { get; }
