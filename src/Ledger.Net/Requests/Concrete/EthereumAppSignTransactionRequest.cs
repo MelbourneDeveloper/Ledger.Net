072859b107583cf9c24384e98a10f1c5db9ad435
Matthew LeLiever <matthew121250@hotmail.com>
2018-09-01 13:53:21 -0400

    Added EthHelpers and start of EthereumAppSignTransactionRequest

--- src/Ledger.Net/Requests/Concrete/EthereumAppSignTransactionRequest.cs
+++ src/Ledger.Net/Requests/Concrete/EthereumAppSignTransactionRequest.cs
@@ -2,20 +2,24 @@
 {
     public class EthereumAppSignTransactionRequest : RequestBase
     {
+        private const byte P1_FIRST_TRANSACTION_BLOCK = 0x00;
+        private const byte P1_SUBSEQUENT_TRANSACTION_BLOCK = 0x80;
+
         #region Public Overrides
-        public override byte Argument1 => 0;
+        public override byte Argument1 => FirstTransactionDataBlock ? P1_FIRST_TRANSACTION_BLOCK : P1_SUBSEQUENT_TRANSACTION_BLOCK;
         public override byte Argument2 => 0;
         public override byte Cla => Constants.CLA;
         public override byte Ins => Constants.ETHEREUM_SIGN_TX;
         #endregion
 
         #region Public Properties
+        public bool FirstTransactionDataBlock { get; }
         #endregion
 
         #region Constructor
-        public EthereumAppSignTransactionRequest(byte[] data) : base(data)
+        public EthereumAppSignTransactionRequest(bool firstTransactionDataBlock, byte[] data) : base(data)
         {
-
+            FirstTransactionDataBlock = firstTransactionDataBlock;
         }
         #endregion
     }
