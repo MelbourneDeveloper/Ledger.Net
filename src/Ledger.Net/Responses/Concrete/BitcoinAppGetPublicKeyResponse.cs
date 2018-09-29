c5d55f2ab9fa74b57a478c8ba4089f0c94078cdd
Matthew LeLiever <matthew121250@hotmail.com>
2018-09-29 12:32:05 -0400

    Refactors to public key response classes
    - Changed public byte data fields to properties in GetPublicKeyResponseBase.
    - Changed abstract methods/properties to only abstract methods.
    - Implemented modified abstract methods into BitcoinAppGetPublicKeyResponse and EthereumAppGetPublicKeyResponse.

--- src/Ledger.Net/Responses/Concrete/BitcoinAppGetPublicKeyResponse.cs
+++ src/Ledger.Net/Responses/Concrete/BitcoinAppGetPublicKeyResponse.cs
@@ -1,37 +1,27 @@
-﻿using System.IO;
-using System.Text;
+﻿using System.Text;
 
 namespace Ledger.Net.Responses
 {
     public class BitcoinAppGetPublicKeyResponse : GetPublicKeyResponseBase
     {
-        public override string PublicKey
-        {
-            get
-            {
-                var sb = new StringBuilder();
-                foreach (var @byte in PublicKeyData)
-                {
-                    sb.Append(@byte.ToString("X2").ToLower());
-                }
-
-                return sb.ToString();
-            }
-        }
-
         public BitcoinAppGetPublicKeyResponse(byte[] data) : base(data)
         {
         }
 
-        protected override string GetAddressFromStream(Stream memoryStream, int addressLength)
+        protected override string GetAddressString(byte[] addressData)
         {
-            var bytes = memoryStream.ReadAllBytes(addressLength);
-            return GetBitcoinAddressStringFromBytes(bytes);
+            return Encoding.ASCII.GetString(addressData);
         }
 
-        private static string GetBitcoinAddressStringFromBytes(byte[] bytes)
+        protected override string GetPublicKeyString(byte[] publicKeyData)
         {
-            return Encoding.ASCII.GetString(bytes);
+            var sb = new StringBuilder();
+            foreach (var @byte in publicKeyData)
+            {
+                sb.Append(@byte.ToString("X2").ToLower());
+            }
+
+            return sb.ToString();
         }
     }
 }
