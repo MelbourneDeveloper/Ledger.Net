b1ce7c98149e35c7575801032c9de18147759cf5
MelbourneDeveloper <christian_findlay@hotmail.com>
2019-03-11 13:36:39 +1100

    a bit closer

--- src/Ledger.Net/Requests/Concrete/BitcoinAppGetPublicKeyRequest.cs
+++ src/Ledger.Net/Requests/Concrete/BitcoinAppGetPublicKeyRequest.cs
@@ -3,22 +3,15 @@
     public class BitcoinAppGetPublicKeyRequest : RequestBase
     {
         #region Public Overrides
-        public override byte Argument1 => (byte)(Display ? 1 : 0);
-        public override byte Argument2 => (byte)BitcoinAddressType;
+        public override byte Argument1 => 0x00;
+        public override byte Argument2 => 0x00;
         public override byte Cla => Constants.CLA;
-        public override byte Ins => Constants.BTCHIP_INS_GET_WALLET_PUBLIC_KEY;
-        #endregion
-
-        #region Public Properties
-        public bool Display { get; }
-        public BitcoinAddressType BitcoinAddressType { get; }
+        public override byte Ins => 0x02;
         #endregion
 
         #region Constructor
-        public BitcoinAppGetPublicKeyRequest(bool display, BitcoinAddressType bitcoinAddressType, byte[] data) : base(data)
+        public BitcoinAppGetPublicKeyRequest( byte[] data) : base(data)
         {
-            Display = display;
-            BitcoinAddressType = bitcoinAddressType;
         }
         #endregion
     }
